65a27ddf573974561a086cfcc9cdd4554baec7ec
Jabbslad <jabbslad@gmail.com>
2025-08-25 17:07:41 +0100

    remove ghostty and starship custom config and fix tailscale monitor

--- install.sh
+++ install.sh
@@ -12,25 +12,17 @@ if ! command -v zls &>/dev/null; then
   mise use -g zls@master
 fi
 mise use -g rust
+mise cache clear
 mise reshim
 
 npm install -g @anthropic-ai/claude-code
 npm install -g @qwen-code/qwen-code
 npm install -g @openai/codex
 
-yay -S --noconfirm --needed cursor-bin zed-bin brave-bin ghostty starship proton-pass-bin python-terminaltexteffects tailscale
+yay -S --noconfirm --needed cursor-bin zed-bin proton-pass-bin python-terminaltexteffects tailscale
 
 # update configs
-sed -i.bak 's/-- alacritty/-- ghostty/' $HOME/.config/hypr/bindings.conf
-sed -i.bak 's/-- chromium/-- brave/' $HOME/.config/hypr/bindings.conf
 sed -i.bak 's/GDK_SCALE,2/GDK_SCALE,1/' $HOME/.config/hypr/monitors.conf
-
-cp ghostty.conf $HOME/.config/ghostty/config
-
-cp starship.toml $HOME/.config/starship.toml
-if ! grep -qxF 'eval "$(starship init bash)"' $HOME/.bashrc; then
-  echo 'eval "$(starship init bash)"' >>$HOME/.bashrc
-fi
 
 cp zig.lua $HOME/.config/nvim/lua/plugins/zig.lua
 
@@ -61,7 +53,8 @@ if ! grep -q "custom/tailscale" ~/.config/waybar/config.jsonc; then
   sed -i '/"battery": {/i\  "custom/tailscale": {\
     "format": "{}",\
     "exec": "~/.local/share/omarchy/bin/omarchy-tailscale-monitor",\
-    "return-type": "json"\
+    "return-type": "json",\
+    "interval": 3\
   },' ~/.config/waybar/config.jsonc
 fi
 
